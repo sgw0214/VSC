a202d1974596fa1520d1c97143199fb93d33f1f5
sgw0214 <sgw0214@hanmail.net>
2020-08-27 22:50:33 +0900

    test

--- TEST.py
+++ TEST.py
@@ -1,2 +1,27 @@
 # git push -u origin master
-완님
+
+#import MySQLdb
+import mysql.connector
+
+def save_record(title, article, date, writer, cnt):
+        # Open database connection
+        db = mysql.connector.connect(host="localhost", user="root", passwd="rnldhs0214",db="test_mysql")
+        db.set_character_set('utf8')
+        # Prepare a cursor object using cursor( method
+        cursor =db.cursor()
+        # # Prepare SQL query to INSERT a record into the database
+        sql= "INSERT INTO document(title, article, wdate, writer, vcnt)\ VALUES (%s, %s, %s, %s, %s)" %("'"+title+"'","'"+article+"'","'"+date+"'","'"+writer+"'", "'"+cnt+"'")
+        try:
+            #Execute the SQL command
+            cursor.execute(sql)
+
+            #Commit changes in the database
+            db.commit()
+            # cursor.execute("""SELECI title, article, date, writer, vcnt FROM document""")
+            # print (cursor.fetchall())
+        except Exception as e:
+            print(str(e))
+            # Rollback in case there is any error******
+            db.rollback()
+            # Disconnect from database
+        db.close()
