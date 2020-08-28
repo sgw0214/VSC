8648ca8ff5b6dd675546c044196d276b47ef36ae
sgw0214 <sgw0214@hanmail.net>
2020-08-28 21:40:48 +0900

    test

--- TEST.py
+++ TEST.py
@@ -3,14 +3,15 @@
 import pymysql
 
 db = pymysql.connect(host="localhost", user="root", passwd="rnldhks0214",db='temp',charset='utf8')
-       
-       
+              
 cur =db.cursor()
         
-sql="create table woan2("\
-    "title varchar(100),"\
-    "content text,"\
-    "primary key(title))"
+#sql="create table woan2("\
+#    "title varchar(100),"\
+#    "content text,"\
+#    "primary key(title))"
+
+sql="delete from woan1"
 
 cur.execute(sql)
 
