4df52cd6eb5a2167e557662d81d46a52ebc27f8a
sgw0214 <sgw0214@hanmail.net>
2020-08-31 23:06:45 +0900

    tst

--- TEST.py
+++ TEST.py
@@ -1,18 +1,60 @@
 # git push -u origin master
 
 import pymysql
+import request
 
 db = pymysql.connect(host="localhost", user="root", passwd="rnldhks0214",db='temp',charset='utf8')
               
 cur =db.cursor()
-        
-#sql="create table woan2("\
-#    "title varchar(100),"\
-#    "content text,"\
-#    "primary key(title))"
 
-sql="delete from woan1"
+def get_sise(stock_code, try_cnt):
+    try:
+        url="http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code={)".format(stock_code)
+        req=urlopen(url)
+        result=req.read()
+        xmlsoup=BeautifulSoup(result, "Ixml-xml")
+        stock = xmlsoup.find("TBL_Stocklnfo")
+        stock_df=pd.DataFrame(stock.attrs, index=[0])
+        stock_df=stock_df.applymap(lambda x: x.replace(",",""))
+        return stock_df
+    except HTTPError as e:
+        logging.warning(e)
+        if try_cnt>=3:
+            return None
+        else:
+            get_sise(stock_code,try_cnt=+1)
+con=sqlite3.connect("./data/div.db")
+stock_code=[['005930','066570']
 
+
+#for s in tqdm(stock_code):
+#    temp=get_sise(s,1)
+ #   temp.to_sql(con=con,name="div_stock_sise",if_exists="append")
+ #   time.sleep(0.5)
+#con.close()
+
+
+#sql="delete from woan1"
+
+'''
+##table 추가   
+sql="create table test("\
+    "title varchar(100),"\
+    "content text(1),"\
+    "primary key(title))"
 cur.execute(sql)
-
 db.commit()
+
+
+##table 삭제
+sql="drop table test"
+cur.execute(sql)
+db.commit()
+
+
+##data 조회
+sql="SELECT * FROM test;"
+cur.execute(sql)
+result=cur.fetchall()
+print(result)
+'''
