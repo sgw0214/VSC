16159418b8f0f74e73462ef085521e2d63c6f1fa
sgw0214 <sgw0214@hanmail.net>
2020-09-01 22:53:54 +0900

    test

--- TEST.py
+++ TEST.py
@@ -1,18 +1,23 @@
 # git push -u origin master
 
 import pymysql
-import request
+import requests
+from urllib.request import urlopen
+import logging
+from bs4 import BeautifulSoup
+import pandas as pd
+from urllib.error import HTTPError
+import time
+from pip._internal import req
 
-db = pymysql.connect(host="localhost", user="root", passwd="rnldhks0214",db='temp',charset='utf8')
-              
+db = pymysql.connect(host="localhost", user="root", passwd="rnldhks0214",db='temp',charset='utf8')           
 cur =db.cursor()
-
 def get_sise(stock_code, try_cnt):
     try:
-        url="http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code={)".format(stock_code)
-        req=urlopen(url)
+        url="http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code={}".format(stock_code)
         result=req.read()
-        xmlsoup=BeautifulSoup(result, "Ixml-xml")
+        req=urlopen(url)
+        xmlsoup=BeautifulSoup(result, "lxml-xml")
         stock = xmlsoup.find("TBL_Stocklnfo")
         stock_df=pd.DataFrame(stock.attrs, index=[0])
         stock_df=stock_df.applymap(lambda x: x.replace(",",""))
@@ -23,9 +28,26 @@ def get_sise(stock_code, try_cnt):
             return None
         else:
             get_sise(stock_code,try_cnt=+1)
-con=sqlite3.connect("./data/div.db")
-stock_code=[['005930','066570']
+    
 
+# 주식 시세 DB에 저장하기
+
+
+stock_code=['005930','066570']
+
+for s in stock_code:
+    temp=get_sise(s,1)
+    temp.to_sql(con=db,name="div_stock_sise", if_exists="append")
+    time.sleep(0.5)
+db.close()
+
+
+
+'''
+rows =db.fetchall()
+for i in rows :
+    print(i)
+'''
 
 
 '''
