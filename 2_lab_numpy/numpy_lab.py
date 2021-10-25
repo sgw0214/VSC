347a757c03e53e2614288f8bef1e6a0cb977c950
sgw0214 <sgw0214@naver.com>
2021-10-25 22:30:17 +0900

    --- Auto-Git Commit ---

--- 2_lab_numpy/numpy_lab.py
+++ 2_lab_numpy/numpy_lab.py
@@ -7,9 +7,14 @@ def n_size_ndarray_creation(n, dtype=np.int):
 print(n_size_ndarray_creation(3))
 
 def zero_or_one_or_empty_ndarray(shape, type, dtype=np.int):
-    x=np.ndarray(shape)
+    if type=="type=0":
+        x=np.ndarray(shape).fillna(0)
+    elif type=="type=1":
+        x=np.ndarray(shape).fillna(1)
+    elif type=="type=99":
+        x=np.ndarray(shape,order='F')
     return x
- print(zero_or_one_or_empty_ndarray(shape=(3,3), type=0))
+zero_or_one_or_empty_ndarray(shape=(2,2), type=1)
 
 def change_shape_of_ndarray(X, n_row):
     pass
