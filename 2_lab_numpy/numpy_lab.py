86623fb13734701c59a785c37732bfb0a38478e9
sgw0214 <sgw0214@naver.com>
2021-10-25 22:22:40 +0900

    --- Auto-Git Commit ---

--- 2_lab_numpy/numpy_lab.py
+++ 2_lab_numpy/numpy_lab.py
@@ -9,7 +9,7 @@ print(n_size_ndarray_creation(3))
 def zero_or_one_or_empty_ndarray(shape, type, dtype=np.int):
     x=np.ndarray(shape)
     return x
- zero_or_one_or_empty_ndarray(shape=(3,3), type=0)
+ print(zero_or_one_or_empty_ndarray(shape=(3,3), type=0))
 
 def change_shape_of_ndarray(X, n_row):
     pass
