fe693dc593fe8ff879c01b733928a869300a8d7c
sgw0214 <sgw0214@naver.com>
2021-11-16 00:05:31 +0900

    --- Auto-Git Commit ---

--- KAGGLE/TITANIC/TITANIC.py
+++ KAGGLE/TITANIC/TITANIC.py
@@ -1,5 +1,18 @@
+from numpy.core.numeric import NaN
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-df=pd.read_csv("E:/VSC/CODE/KAGGLE/TITANIC/test.csv")
-print(df.head())
+
+df=pd.read_csv("E :/VSC/CODE/KAGGLE/TITANIC/train.csv")
+print(df.info())
+
+df1=df
+age_avg=df1.groupby(["Survived","Sex"])["Age"].mean()
+age_avg=age_avg.reset_index()
+# print(age_avg)
+df1["Age_mod"]=df1["Age"].fillna(df1.groupby(["Survived","Sex"])["Age"]. transform('mean'))
+print(df1)
+df1.drop(columns=["Cabin"],inplace=True)
+print(df1)
+
