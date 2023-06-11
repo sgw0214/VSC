9cbabfff8f1dcb0805ddeab279e11960bfec6998
sgw0214 <sgw0214@naver.com>
2023-06-11 22:18:20 +0900

    --- Auto Git Commit ---

--- VR/VR_TEST.py
+++ VR/VR_TEST.py
@@ -19,23 +19,25 @@
 # window.show()
 # app.exec_()
 
-import sys 
-from PyQt5.QtWidgets import *
-from PyQt5.QAxContainer import *
+# import sys 
+# from PyQt5.QtWidgets import *
+# from PyQt5.QAxContainer import *
 
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.ocx.dynamicCall("CommConnect()")
-        self.ocx.OnEventConnect.connect(self.OnEventConnect)
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+#         self.ocx.dynamicCall("CommConnect()")
+#         self.ocx.OnEventConnect.connect(self.OnEventConnect)
 
-    def OnEventConnect(self, err_code):
-        print(err_code)
+#     def OnEventConnect(self, err_code):
+#         print(err_code)
 
 
-app = QApplication(sys.argv)
-window = MyWindow()
-window.show()
-app.exec_()
+# app = QApplication(sys.argv)
+# window = MyWindow()
+# window.show()
+# app.exec_()
+
+set CONDA_FORCE_32BIT=1 
