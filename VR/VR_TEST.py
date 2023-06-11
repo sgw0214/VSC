53f2ff489bc615de091f1c9d301f1e5187189d61
sgw0214 <sgw0214@naver.com>
2023-06-11 22:11:47 +0900

    --- Auto Git Commit ---

--- VR/VR_TEST.py
+++ VR/VR_TEST.py
@@ -1,1 +1,41 @@
-1+1
+# import sys 
+# from PyQt5.QtWidgets import *
+
+# app = QApplication(sys.argv)
+# label = QLabel("Hello PyQt")
+# label.show()
+# app.exec_()
+
+# import sys 
+# from PyQt5.QtWidgets import *
+
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+
+
+# app = QApplication(sys.argv)
+# window = MyWindow()
+# window.show()
+# app.exec_()
+
+import sys 
+from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.ocx.dynamicCall("CommConnect()")
+        self.ocx.OnEventConnect.connect(self.OnEventConnect)
+
+    def OnEventConnect(self, err_code):
+        print(err_code)
+
+
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec_()
