9d41abfd6e0bc5ccb9b089eb9ac1ec14a5e76279
sgw0214 <sgw0214@naver.com>
2023-06-12 00:58:20 +0900

    --- Auto Git Commit ---

--- VR/VR_TEST.py
+++ VR/VR_TEST.py
@@ -2,4 +2,8 @@
 # set CONDA_FORCE_32BIT=1 
 # conda create -n base python=3.8.5
 
-print(1+16)
+from pykiwoom.kiwoom import *
+
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
+print("블록킹 로그인 완료")
