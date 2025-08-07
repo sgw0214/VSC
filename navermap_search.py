de844293f086b520150e7e417154b384d766ab35
sgw0214 <sgw0214@naver.com>
2025-08-07 13:10:41 +0900

    --- Auto Git Commit ---

--- navermap_search.py
+++ navermap_search.py
@@ -143,6 +143,7 @@ def search_lnglat(key_word):
                     print(road_address_fi)
                     
                 else:  
+                    data=key_word
                     # 주소 버튼 누르기
                     address_buttons = driver.find_elements(By.CSS_SELECTOR, '.PkgBl> a')
                     address_buttons[data].click()    
