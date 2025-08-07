2c2e7a896708a3616a1b4c81a7e58a0a344e26da
sgw0214 <sgw0214@naver.com>
2025-08-07 13:40:11 +0900

    --- Auto Git Commit ---

--- navermap_search.py
+++ navermap_search.py
@@ -146,8 +146,8 @@ def search_lnglat(key_word):
                 else:  
                     data=key_word
                     # 주소 버튼 누르기
-                    address_buttons = driver.find_elements(By.CSS_SELECTOR, '.vV_z_ > div')
-                    address_buttons[0].click()    
+                    address_buttons = driver.find_element(By.CSS_SELECTOR, '.vV_z_ > a')
+                    address_buttons.click()    
                     # 로딩 기다리기
                     sleep(2)       
                     # 주소 눌렀을 때 도로명, 지번 나오는 div
@@ -242,7 +242,7 @@ def time_filter(timetext):
 
 df=DataFrame()
 point_hist=DataFrame()
-point_list=["하슬라아트월드","탄현마을부영3단지아파트상가","일산동양아파트101동","대윤프라자","탄현큰마을대림아파트 일현로 140","광성교회","리드인 독서논술 일산","일산에듀포레푸르지오아파트","SK엔크린 삼정셀프주유소"]
+point_list=["탄현마을부영3단지아파트상가","일산동양아파트101동","대윤프라자","탄현큰마을대림아파트 일현로 140","광성교회","리드인 독서논술 일산","일산에듀포레푸르지오아파트","SK엔크린 삼정셀프주유소"]
 # point_list=["엘지디스플레이 파주공장","일산동양아파트101동","대윤프라자","탄현큰마을대림아파트 일현로 140","광성교회","리드인 독서논술 일산","탄현마을부영3단지아파트","일산에듀포레푸르지오아파트","SK엔크린 삼정셀프주유소"]
 df=DataFrame(index=point_list,columns=point_list)
 point_hist=DataFrame(index=point_list,columns=["lng","lat"])
